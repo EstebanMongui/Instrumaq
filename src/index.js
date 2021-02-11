59988df6df1d30df5b6da6b04924f7dfa73944cb
Esteban Monguí <esteband.itorres@gmail.com>
2021-02-11 09:54:26 -0500

    Strat Instrumaq V1.0

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//Components}
+import Home from "./pages/Home/Home.jsx";
+
+const container = document.getElementById('App');
+
+ReactDOM.render(<Home />, container);
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.scss";
+//Components
+
+class Home extends React.Component{
+    render(){
+        return(
+            <section className="Home">
+
+            </section>
+        )
+    }
+}
+
+export default Home;
