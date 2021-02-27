1beb0831389bcdadee151daf28cc26348eb85a2b
Esteban Monguí <esteband.itorres@gmail.com>
2021-02-27 10:01:19 -0500

    feat: footer V1.0

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss"
+
+const Footer = () =>{
+    return(
+        <div className="Footer">
+            <figure className="Footer__Logo"><img src="" alt="Logo"/></figure>
+            <h3>instrumaqsas@gmail.com</h3>
+            <h3>301 232 9734</h3>
+            <h3>Bogotá-Colombia</h3>
+        </div>
+    )
+}
+export default Footer;
